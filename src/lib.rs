f3c7e9824d75835a2ef058f65bb59fddead79a55
LambdaXymox <lambda.xymox@gmail.com>
2016-09-01 18:22:56 -0500

    Add some documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+/*!
+This crate contains extra methods for working with hashmaps. In particular, it contains the
+hashmap! macro for directly creating hash maps.
+*/
 #![crate_name="hashmap"]
 
 #[macro_use]
